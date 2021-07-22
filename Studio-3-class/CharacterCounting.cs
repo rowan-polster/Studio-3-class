80117df2c0699ee426453059d7448fa16c3c0f5d
Rowan Polster <rowandownriver94@gmail.com>
2021-07-21 21:00:12 -0500

    Add project files.

---
+++ Studio-3-class/CharacterCounting.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Studio_3_class
+{
+    class CharacterCounting
+    {
+        public void FindChars(string sentence)
+        {
+            sentence = String.Concat(Array.FindAll(sentence.ToCharArray(), Char.IsLetterOrDigit));
+            char[] charactersInString = sentence.ToLower().ToCharArray();
+            var dictionary = new Dictionary<char, int>();
+            
+
+            foreach(char character in charactersInString)
+            {
+                int count = sentence.ToLower().Split(character).Length - 1;  
+
+                if (!dictionary.ContainsKey(character))
+                {
+                    dictionary.Add(character, count);
+                }
+            }
+
+            foreach(var pair in dictionary)
+            {
+                Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+            }
+        }
+
+    }
+}
---
+++ Studio-3-class/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Studio_3_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is a practice sentence!!!!";
+
+            Console.WriteLine("Please enter a sentence for character counting:\n");
+            string inputSentence = Console.ReadLine();
+
+            var charactersInString = new CharacterCounting();
+
+            charactersInString.FindChars(inputSentence);
+        }
+    }
+}
